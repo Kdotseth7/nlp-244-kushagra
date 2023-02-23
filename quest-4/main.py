e20e8d4725ddf3aa86e389207b22af1213f891fb
Kushagra Seth <kushagraseth.1996@gmail.com>
2023-02-22 21:55:28 -0800

    removed warning for max length

--- quest-4/main.py
+++ quest-4/main.py
@@ -1,6 +1,7 @@
 import torch
 import os
 import datasets
+import optparse
 from torch.utils.data import Dataset, DataLoader
 from utils import get_device
 from data import get_data
@@ -11,7 +12,12 @@ from transformers import (T5Config,
                           TrainingArguments, 
                           Trainer)
 from translate import SNLIDataset
+from functools import partial
 
+optparser = optparse.OptionParser()
+optparser.add_option("-b", "--batch-size", dest="batch_size", default=1024, type="int", help="Size of each batch")
+optparser.add_option("-n", "--num-workers", dest="num_workers", default=0, type="int", help="Number of workers to use for dataloader")
+(opts, _) = optparser.parse_args()
 
 def translate(loader: DataLoader, tokenizer: T5Tokenizer, model: T5ForConditionalGeneration, dataset_type: str) -> list:
     data_dict = dict()
@@ -34,8 +40,8 @@ def translate(loader: DataLoader, tokenizer: T5Tokenizer, model: T5ForConditiona
 
 def custom_collate_fn(batch, tokenizer: T5Tokenizer) -> tuple:
     premise_inputs, hypothesis_inputs, labels = zip(*batch)
-    premise_input_ids = tokenizer.batch_encode_plus(premise_inputs, padding=True, return_tensors='pt')['input_ids'].to(get_device())
-    hypothesis_input_ids = tokenizer.batch_encode_plus(hypothesis_inputs, padding=True, return_tensors='pt')['input_ids'].to(get_device())
+    premise_input_ids = tokenizer.batch_encode_plus(premise_inputs, max_length=512, padding=True, truncation=True, return_tensors='pt')['input_ids'].to(get_device())
+    hypothesis_input_ids = tokenizer.batch_encode_plus(hypothesis_inputs, max_length=512, padding=True, truncation=True, return_tensors='pt')['input_ids'].to(get_device())
     labels = torch.tensor(labels).to(get_device())
     return premise_input_ids, hypothesis_input_ids, labels
 
@@ -43,20 +49,28 @@ if __name__ == "__main__":
     # Set Device
     device = get_device()
     
+    # Set Multiprocessing
+    if opts.num_workers > 0:
+        torch.multiprocessing.set_start_method('spawn')
+    
     # Load Data
     train, dev, test = get_data("snli")
 
+    # Load Config, Tokenizer, Model
     config = T5Config.from_pretrained("t5-small")
-    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    tokenizer = T5Tokenizer.from_pretrained("t5-small", model_max_length=512)
     model = T5ForConditionalGeneration.from_pretrained("t5-small")  
     model.to(device)
     
+    # Create Datasets
     train_ds = SNLIDataset(train)
     dev_ds = SNLIDataset(dev)
     test_ds = SNLIDataset(test)
-    train_loader = DataLoader(train_ds, batch_size=1024, shuffle=False, collate_fn=lambda batch: custom_collate_fn(batch, tokenizer))
-    dev_loader = DataLoader(dev_ds, batch_size=1024, shuffle=False, collate_fn=lambda batch: custom_collate_fn(batch, tokenizer))
-    test_loader = DataLoader(test_ds, batch_size=1024, shuffle=False, collate_fn=lambda batch: custom_collate_fn(batch, tokenizer))
+    
+    # Create Dataloaders
+    train_loader = DataLoader(train_ds, batch_size=opts.batch_size, num_workers=opts.num_workers, shuffle=False, collate_fn=partial(custom_collate_fn, tokenizer=tokenizer))
+    dev_loader = DataLoader(dev_ds, batch_size=opts.batch_size, num_workers=opts.num_workers, shuffle=False, collate_fn=partial(custom_collate_fn, tokenizer=tokenizer))
+    test_loader = DataLoader(test_ds, batch_size=opts.batch_size, num_workers=opts.num_workers, shuffle=False, collate_fn=partial(custom_collate_fn, tokenizer=tokenizer))
     
     # Save the dataset to disk
     dataset_cache_path: str = "./data/fnli"
@@ -64,9 +78,11 @@ if __name__ == "__main__":
     dev_path = os.path.join(dataset_cache_path, "dev_dataset")
     test_path = os.path.join(dataset_cache_path, "test_dataset")
     if not os.path.exists(dataset_cache_path):
-        train_fnli = translate(train_loader, tokenizer, model, "Train")
-        dev_fnli = translate(dev_loader, tokenizer, model, "Dev")
-        test_fnli = translate(test_loader, tokenizer, model, "Test")
+        # Translate the dataset using T5
+        with torch.no_grad():
+            train_fnli = translate(train_loader, tokenizer, model, "Train")
+            dev_fnli = translate(dev_loader, tokenizer, model, "Dev")
+            test_fnli = translate(test_loader, tokenizer, model, "Test")
         train_fnli.save_to_disk(train_path)
         dev_fnli.save_to_disk(dev_path)
         test_fnli.save_to_disk(test_path)
@@ -74,3 +90,7 @@ if __name__ == "__main__":
         train_fnli = datasets.load_from_disk(train_path)
         dev_fnli = datasets.load_from_disk(dev_path)
         test_fnli = datasets.load_from_disk(test_path)
+        
+    print(train_fnli)
+    print(dev_fnli)
+    print(test_fnli)
